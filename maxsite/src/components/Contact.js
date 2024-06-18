fe5ea49f7dacc27c0e8a628c7b82f62e6aca60f0
maxx06 <67605840+maxx06@users.noreply.github.com>
2024-06-18 15:20:35 -0700

    basic layout

---
+++ maxsite/src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Contact(){
+    return (
+        <div>
+            <ul>
+                <li>Email (preferred)</li>
+                <li>Phone</li>
+                <li>LinkedIn</li>
+            </ul>
+        </div>
+    );
+}
---
+++ maxsite/src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Main() {
+    return (
+        <div>
+        <h1>Maxsite</h1>
+        <p>Maxsite is a simple website that uses React.</p>
+        </div>
+    );
+
+}
