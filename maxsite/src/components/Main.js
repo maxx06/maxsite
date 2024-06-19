ce2c39859b33dc21e06b2288684a32d364fb73e3
maxx06 <67605840+maxx06@users.noreply.github.com>
2024-06-18 23:46:34 -0700

    techbro

--- maxsite/src/components/Main.js
+++ maxsite/src/components/Main.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 export default function Main() {
     return (
-        <div>
+        <div className="intro">
             <p>hi. i'm</p>
             <h1 className="main--name">max xiong.</h1>
             <p>artificial intelligence. front-end. UIUX. photography. boba</p>
+            <img src="../images/techbro.png" alt="TechBro" />
         </div>
     );
 
