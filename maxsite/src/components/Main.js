a717d0471799716a90002f650783721d92088f94
maxx06 <67605840+maxx06@users.noreply.github.com>
2024-06-19 12:41:30 -0700

    fixed img bug

--- maxsite/src/components/Main.js
+++ maxsite/src/components/Main.js
@@ -6,7 +6,7 @@ export default function Main() {
             <p>hi. i'm</p>
             <h1 className="main--name">max xiong.</h1>
             <p>artificial intelligence. front-end. UIUX. photography. boba</p>
-            <img src="../images/techbro.png" alt="TechBro" />
+            <img src="/images/tb.png" alt="TechBro" height="400"></img>
         </div>
     );
 
