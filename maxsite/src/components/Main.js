6bf279bf65ac022966ee46cfbab89787cd89a88b
maxx06 <67605840+maxx06@users.noreply.github.com>
2024-06-20 23:01:28 -0700

    button

--- maxsite/src/components/Main.js
+++ maxsite/src/components/Main.js
@@ -8,7 +8,7 @@ export default function Main() {
                 <p>hi. i'm</p>
                 <h1 className="main--name">max xiong.</h1>
                 <p>artificial intelligence. front-end. UIUX. photography. boba</p>
-                <button className="button-64">Contact Me</button>
+                <button className="button-64">view projects          →</button>
             </div>
             
             <img className="techbropic" src="/images/tb.png" alt="TechBro" height="350"></img>
