012ceb9f530d530ff2c3315c776253d69fdb1cc0
maxx06 <67605840+maxx06@users.noreply.github.com>
2024-06-22 11:54:36 -0700

    cleanup

---
+++ maxsite/src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div className="intro">
+            <div>
+                <div>
+                    <img src="boba.png"></img>
+                </div>
+                <div>
+                    <h1>max</h1>
+                    <p>Hi, I'm Max! I am a freshman at Duke University studying computer science and math interested in artificial intelligence. </p>
+                </div>
+                
+            </div>
+        </div>
+    );
+}
